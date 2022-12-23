915aa333ae236d1d8491ba636a49fa1227e17363
Alex <alex@codingcat.dev>
2022-12-23 11:51:10 -0500

    add page links

--- apps/codingcatdev/src/lib/components/layout/Footer.svelte
+++ apps/codingcatdev/src/lib/components/layout/Footer.svelte
@@ -3,4 +3,10 @@
 		<span class="footer-title">Socials</span>
 		<a href="https://youtube.com/codingcatdev" class="link link-hover">Branding</a>
 	</div>
+	<div>
+		<span class="footer-title">Helpful Links</span>
+		<a href="/ftc-disclosure" class="link link-hover">FTC Disclosure</a>
+		<a href="/privacy-policy" class="link link-hover">Privacy Policy</a>
+		<a href="/terms-of-use" class="link link-hover">Terms of Use</a>
+	</div>
 </footer>
