48dcc816acf4218988bdcc36800954fe1d6c49e7
Alex <alex@codingcat.dev>
2022-12-26 17:17:22 -0500

    add 404 page

--- apps/codingcatdev/src/routes/[slug]/+page.server.js
+++ apps/codingcatdev/src/routes/[slug]/+page.server.js
@@ -1,10 +1,15 @@
 import { getContentBySlug } from '$lib/server/content';
 import { ContentType } from '$lib/types';
+import { error } from '@sveltejs/kit';
 /**
  * @type {import('./$types').PageServerLoad}
  * */
 export async function load({ params }) {
-	return {
-		content: await getContentBySlug(ContentType.page, params.slug)
-	};
+	const content = await getContentBySlug(ContentType.page, params.slug);
+	if (!content) {
+		throw error(404, {
+			message: 'Not found'
+		});
+	}
+	return content;
 }
