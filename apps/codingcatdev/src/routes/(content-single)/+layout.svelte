f07ed0461062a81f648229956ef3415c1bc89abe
Alex <alex@codingcat.dev>
2022-12-26 16:09:24 -0500

    move layout data up one level

--- apps/codingcatdev/src/routes/(content-single)/+layout.svelte
+++ apps/codingcatdev/src/routes/(content-single)/+layout.svelte
@@ -9,6 +9,9 @@
 <section class="content-single p-1 xl:p-8 sm:flex justify-center">
 	<div><slot /></div>
 	<div class="hidden xl:block">
+		<RecentPostsList contentType={ContentType.course} list={data.course} />
+		<RecentPostsList contentType={ContentType.tutorial} list={data.tutorial} />
+		<RecentPostsList contentType={ContentType.podcast} list={data.podcast} />
 		<RecentPostsList contentType={ContentType.post} list={data.post} />
 	</div>
 </section>
