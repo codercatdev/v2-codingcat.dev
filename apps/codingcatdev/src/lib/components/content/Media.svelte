81bcdc44c2f978969cb7681e079ef1fab63832e8
Alex <alex@codingcat.dev>
2023-01-06 17:23:27 -0500

    media working

---
+++ apps/codingcatdev/src/lib/components/content/Media.svelte
@@ -0,0 +1,45 @@
+<script>
+	import videojs from 'video.js';
+	// import 'videojs-youtube';
+	import { onMount, onDestroy } from 'svelte';
+	onMount(() => {
+		videojs(
+			'video-player',
+			{
+				controls: true,
+				autoplay: false,
+				preload: 'auto',
+				fluid: true
+				// techOrder: ['html5', 'youtube']
+				// sources:
+			},
+			undefined
+		);
+	});
+	onDestroy(() => {
+		for (const player of videojs.getAllPlayers()) {
+			player.dispose();
+		}
+	});
+</script>
+
+<svelte:head>
+	<link
+		type="text/css"
+		rel="stylesheet"
+		href="../../../../node_modules/video.js/dist/video-js.min.css"
+	/>
+</svelte:head>
+
+<!-- svelte-ignore a11y-media-has-caption -->
+<!-- <video bind:this={vid} /> -->
+<video id="video-player" class="video-js">
+	<source src="//vjs.zencdn.net/v/oceans.mp4" />
+
+	<p class="vjs-no-js">
+		To view this video please enable JavaScript, and consider upgrading to a web browser that
+		<a href="https://videojs.com/html5-video-support/" target="_blank" rel="noreferrer"
+			>supports HTML5 video</a
+		>
+	</p>
+</video>
--- apps/codingcatdev/src/routes/(content-single)/+layout.svelte
+++ apps/codingcatdev/src/routes/(content-single)/+layout.svelte
@@ -1,12 +1,16 @@
 <script>
 	import { ContentType } from '$lib/types/index';
 	import RecentPostsList from '$lib/components/RecentPostsList.svelte';
+	import Media from '$lib/components/content/Media.svelte';
 
 	/** @type {import('./$types').LayoutData} */
 	export let data;
 </script>
 
-<section class="content-single p-1 xl:p-8 sm:flex justify-center">
+<!-- TODO: Fix layout -->
+<Media />
+
+<section class="justify-center p-1 content-single xl:p-8 sm:flex">
 	<div><slot /></div>
 	<div class="hidden xl:block">
 		<RecentPostsList contentType={ContentType.course} list={data.course} />
